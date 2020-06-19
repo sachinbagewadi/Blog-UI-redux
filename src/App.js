ad8017ad38583f39cc8ef3f94d4552cca0e46666
sachinbagewadi <66544809+sachinbagewadi@users.noreply.github.com>
2020-06-19 13:08:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {BrowserRouter,Link,Route} from 'react-router-dom'
+import Home from './component/home'
+import Users from './component/Users'
+import UserShow from './component/UserShow'
+import Posts from './component/Posts'
+import PostsShow from './component/PostsShow'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+          <Link to="/">Home||</Link>
+          <Link to="/users">Users||</Link> 
+          <Link to="/posts">Posts||</Link>
+
+          <Route path="/" component={Home} exact={true}/>
+          <Route path="/users" component={Users} exact={true}/>
+          <Route path="/users/:id" component={UserShow} />
+          <Route path="/posts" component={Posts} exact={true}/>
+          <Route path="/posts/:id" component={PostsShow} />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
